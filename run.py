e1807d3be07fa2d4a71e838747522da6417e18a7
Victor <cider.sweet@yandex.ru>
2020-08-22 23:12:25 +0300

    rq debug

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app import create_app
 
-# app = create_app()
-# app.app_context().push()
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(use_reloader=False, debug=False)
-    app.app_context().push()
+app = create_app()
+# app.run(use_reloader=False, debug=False)
+app.app_context().push()
