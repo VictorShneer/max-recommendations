d31c9830198aab9e8b91603be50e0d3bac66ab76
ykvlw <32446844+ykvlw@users.noreply.github.com>
2020-08-23 05:03:06 +0300

    Начал аналитику

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -3,18 +3,31 @@ from flask_login import login_required, current_user
 from app.models import User, Integration
 from app import db
 from app.analytics import bp
-from app.analytics.utils import current_user_own_integration
-
-
-@bp.route('/analytics/<integration_id>/get_data')
-@login_required
-def analytics_get_data():
-    # TODO Getting the data for the table
-    pass
+from app.analytics.utils import current_user_own_integration, create_url_for_query, send_request_to_clickhouse
+import traceback
+from pprint import pprint
+import pandas as pd
 
 
 @bp.route('/analytics/<integration_id>', methods = ['GET'])
 @login_required
 @current_user_own_integration
-def analytics(integration_id):
-    return render_template('analytics.html')
+def generate_values(integration_id):
+    try:
+        # Getting the data needed for the drop down menu
+        list_of_column_names = ['DeviceCategory','OperatingSystem','RegionCity','URL']
+        list_of_answers = []
+        for i in range(len(list_of_column_names)):
+            create_url = create_url_for_query('SELECT {smth} FROM db1.hits_georgefinal GROUP BY {smth2};'.format(smth=list_of_column_names[i],smth2=list_of_column_names[i]))
+            get_data = send_request_to_clickhouse(create_url).text
+            list_of_answers.append(get_data)
+            i += 1
+        # Generating readable data for the drop down menu
+        df = pd.DataFrame(list_of_answers, columns=['Values'])
+        a = df.Values.str.split("\n")
+
+    except Exception as e:
+        traceback.print_exc()
+        flash('{} Ошибки в настройках интеграции!'.format(Integration.integration_name))
+        return render_template('analytics.html')
+    return render_template('analytics.html', list = a)
--- app/analytics/utils.py
+++ app/analytics/utils.py
@@ -1,15 +1,27 @@
 from app.models import User, Integration
 from flask_login import current_user
 from flask import abort
+import requests
+from flask import current_app
 
 def current_user_own_integration(function):
     def wrapper(integration_id):
         integration = Integration.query.filter_by(id=integration_id).first_or_404()
-        print(integration)
         if current_user.id != integration.user_id:
             print('Permission abort')
             abort(404)
         else:
-            function(integration_id)
-            return print("SUCCESS")
+            return function(integration_id)
     return wrapper
+
+def create_url_for_query(query):
+    host = current_app.config['CLICKHOUSE_HOST']
+    db = current_app.config['CLICKHOUSE_DB']
+    return 'https://{host}:8443/?database={db}&query={query}'.format(host=host, db=db, query=query)
+
+def send_request_to_clickhouse(url):
+    certificate_path = 'app/YandexInternalRootCA.crt'
+    auth = {'X-ClickHouse-User': current_app.config['CLICKHOUSE_LOGIN'],'X-ClickHouse-Key': current_app.config['CLICKHOUSE_PASSWORD']
+    }
+    r = requests.get(url = url, headers=auth, verify=certificate_path)
+    return r
