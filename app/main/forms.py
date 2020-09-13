f7043d968e6a9b9e6134599fb0e5fe1b0dafc1b1
Victor <cider.sweet@yandex.ru>
2020-09-13 17:09:23 +0300

    validate api_key input

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,17 +4,22 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import User, Integration
 from datetime import date
 from wtforms.fields.html5 import DateField
-
+import requests
 class EditIntegration(FlaskForm):
     integration_name = StringField("Название интеграции", validators=[DataRequired()])
-    api_key = StringField('API ключ GetResponse')
+    api_key = StringField('API ключ GetResponse', validators=[DataRequired()])
     user_domain = StringField('Домен GetResponse')
-    metrika_key = StringField('Ключ Яндекс Метрики')
-    metrika_counter_id = StringField('ID счетчика Яндекс Метрики')
+    metrika_key = StringField('Ключ Яндекс Метрики', validators=[DataRequired()])
+    metrika_counter_id = StringField('ID счетчика Яндекс Метрики', validators=[DataRequired()])
     start_date = DateField('От', validators=[DataRequired()])
     auto_load = BooleanField('Автозагрузка')
     submit = SubmitField("Отправить")
 
+    def validate_api_key(self, api_key):
+        r = requests.get('https://api.getresponse.com/v3/accounts', \
+                    headers={'X-Auth-Token': 'api-key {}'.format(api_key.data)})
+        if r.status_code != 200 :
+            raise ValidationError(('Нет контакта с GetResponse'))
 
 class LinkGenerator(FlaskForm):
     link = StringField("Введите ссылку", validators=[DataRequired()])
--- app/main/routes.py
+++ app/main/routes.py
@@ -82,7 +82,7 @@ def create_integration():
         flash('Настройка вашего аккаунта еще не закончена')
         return redirect(url_for('main.user_integrations'))
     form = EditIntegration()
-    if form.validate_on_submit():
+    if form.validate_on_submit():            
         integration = Integration(
         integration_name = form.integration_name.data,
         api_key = form.api_key.data,
