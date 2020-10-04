26e84a468ecf450daf7697f7f672e7e25a5af7da
cider_sweet <victor.zakharenko@yahoo.com>
2020-10-04 22:12:16 +0300

    hmm what?

---
+++ app/admin/admin_security.py
@@ -0,0 +1,40 @@
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView, expose
+from flask_login import current_user
+from flask import current_app
+from flask import redirect, url_for
+from app.admin.forms import AssignCryptoForm
+from app import db
+
+class MyModelView(ModelView):
+    def __init__(self, model, session, name=None, category=None, endpoint=None, url=None, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        super(MyModelView, self).__init__(model, session, name=name, category=category, endpoint=endpoint, url=url)
+
+    def is_accessible(self):
+        current_roles = [role.name for role in current_user.roles]
+        return (current_user.is_authenticated and current_app.config['ADMIN_ROLE_TITLE'] in current_roles)
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login'))
+
+class MyAdminIndexView(AdminIndexView):
+    @expose('/', methods=['GET', 'POST'])
+    def index(self):
+        form = AssignCryptoForm()
+        if form.validate_on_submit():
+            user_id = form.user_id.data
+            crypto = form.crypto.data
+            current_user.launch_task('assign_crypto_to_user_id',\
+                                    'Присваиваю крипто и создаю базы в кликхаусе.', \
+                                    current_user.id, user_id,crypto)
+            db.session.commit()
+        return self.render('admin/adminindex.html', form=form)
+
+    def is_accessible(self):
+        current_roles = [role.name for role in current_user.roles]
+        return (current_user.is_authenticated and current_app.config['ADMIN_ROLE_TITLE'] in current_roles)
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login'))
---
+++ app/admin/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import ValidationError, DataRequired
+from app.models import User
+
+
+
+class AssignCryptoForm(FlaskForm):
+
+    user_id = IntegerField('Id Пользователя', validators=[DataRequired()])
+    crypto = StringField('Имя', validators=[DataRequired()])
+    submit = SubmitField('Go!')
+
+    def validate_user_id(self, user_id):
+        user = User.query.filter_by(id=user_id.data).first()
+        if not user:
+            raise ValidationError('С таким ID нет пользователя')
