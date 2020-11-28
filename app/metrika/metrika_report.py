12f3ab51b22f376e967840397905cac7457a29ce
cider_sweet <victor.zakharenko@yahoo.com>
2020-11-28 19:13:58 +0200

    prev

--- app/metrika/metrika_report.py
+++ app/metrika/metrika_report.py
@@ -18,8 +18,8 @@ class MetrikaReport(object):
         self.time_series_goals_df = time_series_goals_df
         clientid_convers_df = clientid_convers_df.loc[clientid_convers_df['Email'].apply(type) == str]
         clientid_convers_df.dropna(subset= ['Email'], inplace=True)
-        no_email_mask = ~self.clientid_convers_df['Email'].str.contains(self.no_email_regex)
-        print(no_email_mask)
+        no_email_mask = self.clientid_convers_df['Email'].str.contains(self.no_email_regex, na=True)
+        print(no_email_mask[no_email_mask.apply(type) != bool])
         no_email_mask = no_email_mask[no_email_mask.apply(lambda x: isinstance(x, bool))]
         print(no_email_mask)
         self.email_visits_slice_df = self.clientid_convers_df[no_email_mask]
